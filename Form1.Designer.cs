56389ee89fdc1347ef8d936badebed41eea195d8
Auzie <105322594+Ozwaldoe@users.noreply.github.com>
2022-12-22 11:07:27 -0800

    Add files via upload

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -33,6 +33,8 @@
             this.gayBiker = new System.Windows.Forms.PictureBox();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.gayBiker)).BeginInit();
             this.SuspendLayout();
             // 
@@ -52,9 +54,9 @@
             this.gayBiker.ErrorImage = ((System.Drawing.Image)(resources.GetObject("gayBiker.ErrorImage")));
             this.gayBiker.Image = ((System.Drawing.Image)(resources.GetObject("gayBiker.Image")));
             this.gayBiker.InitialImage = ((System.Drawing.Image)(resources.GetObject("gayBiker.InitialImage")));
-            this.gayBiker.Location = new System.Drawing.Point(-236, -221);
+            this.gayBiker.Location = new System.Drawing.Point(-264, -214);
             this.gayBiker.Name = "gayBiker";
-            this.gayBiker.Size = new System.Drawing.Size(1466, 905);
+            this.gayBiker.Size = new System.Drawing.Size(1193, 905);
             this.gayBiker.TabIndex = 2;
             this.gayBiker.TabStop = false;
             // 
@@ -78,11 +80,33 @@
             this.textBox2.Size = new System.Drawing.Size(316, 42);
             this.textBox2.TabIndex = 4;
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(573, 243);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Default";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(170, 243);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Load";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button2);
@@ -100,5 +124,7 @@
         private PictureBox gayBiker;
         private TextBox textBox1;
         private TextBox textBox2;
+        private Button button1;
+        private Button button3;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -1,11 +1,15 @@
 using Microsoft.VisualBasic;
 using Microsoft.VisualBasic.ApplicationServices;
 using System.IO;
+using System.Text;
 
 namespace Save_Delete
 {
     public partial class Form1 : Form
     {
+
+        private string text = "\"steam_autocloud.vdf\"" + "\n{\n" + "    \"accountid\"" + "   \"69420\"" + "\n}"; // 
+
         public Form1()
         {
             InitializeComponent();
@@ -13,22 +17,42 @@ namespace Save_Delete
 
         private void button2_Click(object sender, EventArgs e) // delete
         {
-            File.Delete(findText);
+            string steamshitpath = findPath.Remove(54);
+            string steampathapi = steamshitpath + @"\steam_autocloud.vdf";
+            File.Delete(steampathapi);
+            File.Delete(findPath);
+
+            using (StreamWriter sw = File.CreateText(steampathapi)) {
+                sw.WriteLine(text);
+            }
         }
 
-        static string username()
+        private void textBox1_TextChanged(object sender, EventArgs e) // info box
         {
-            return Environment.UserName;
+            textBox1.Text = "enter your save location in the box below";
         }
 
-        private void textBox1_TextChanged(object sender, EventArgs e)
-        {
-            textBox1.Text = "Please enter your save location in the box below";
-        }
-
-        public string findText
+        public string findPath // finds the info inputted to the text box
         {
             get { return textBox2.Text; }
+        }
+
+        private void button1_Click(object sender, EventArgs e) // button to set the default path
+        {
+            defaultPath();
+        }
+
+        public void defaultPath() // sets the default path
+        {
+            string name = Environment.UserName;
+            textBox2.Text = @$"C:\Users\{name}\Documents\My Games\HotlineMiami\SaveData.sav";
+        }
+
+        private void button3_Click(object sender, EventArgs e) // Load Path
+        {
+            string textFile = Environment.CurrentDirectory + @"/Config/Path.txt";
+            string text = File.ReadAllText(textFile);
+            textBox2.Text = text;
         }
     }
 }
