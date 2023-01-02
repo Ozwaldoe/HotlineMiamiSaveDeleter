af987afb71b78cef940ca95bdc6d293e452b95cc
Auzie <105322594+Ozwaldoe@users.noreply.github.com>
2023-01-02 19:29:35 +0000

    Add files via upload

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -41,9 +41,10 @@
             // button2
             // 
             this.button2.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.button2.Location = new System.Drawing.Point(251, 294);
+            this.button2.Location = new System.Drawing.Point(287, 392);
+            this.button2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(316, 144);
+            this.button2.Size = new System.Drawing.Size(361, 192);
             this.button2.TabIndex = 1;
             this.button2.Text = "Delete Save";
             this.button2.UseVisualStyleBackColor = true;
@@ -54,37 +55,41 @@
             this.gayBiker.ErrorImage = ((System.Drawing.Image)(resources.GetObject("gayBiker.ErrorImage")));
             this.gayBiker.Image = ((System.Drawing.Image)(resources.GetObject("gayBiker.Image")));
             this.gayBiker.InitialImage = ((System.Drawing.Image)(resources.GetObject("gayBiker.InitialImage")));
-            this.gayBiker.Location = new System.Drawing.Point(-264, -214);
+            this.gayBiker.Location = new System.Drawing.Point(-208, -225);
+            this.gayBiker.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.gayBiker.Name = "gayBiker";
-            this.gayBiker.Size = new System.Drawing.Size(1193, 905);
+            this.gayBiker.Size = new System.Drawing.Size(1155, 835);
             this.gayBiker.TabIndex = 2;
             this.gayBiker.TabStop = false;
             // 
             // textBox1
             // 
             this.textBox1.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.textBox1.Location = new System.Drawing.Point(251, 162);
+            this.textBox1.Location = new System.Drawing.Point(287, 216);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
             this.textBox1.ReadOnly = true;
-            this.textBox1.Size = new System.Drawing.Size(316, 47);
+            this.textBox1.Size = new System.Drawing.Size(361, 61);
             this.textBox1.TabIndex = 3;
             this.textBox1.Text = "Please enter your save location in the box below";
             this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(251, 234);
+            this.textBox2.Location = new System.Drawing.Point(287, 312);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.textBox2.Multiline = true;
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(316, 42);
+            this.textBox2.Size = new System.Drawing.Size(361, 55);
             this.textBox2.TabIndex = 4;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(573, 243);
+            this.button1.Location = new System.Drawing.Point(655, 324);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(86, 31);
             this.button1.TabIndex = 5;
             this.button1.Text = "Default";
             this.button1.UseVisualStyleBackColor = true;
@@ -92,9 +97,10 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(170, 243);
+            this.button3.Location = new System.Drawing.Point(194, 324);
+            this.button3.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.Size = new System.Drawing.Size(86, 31);
             this.button3.TabIndex = 6;
             this.button3.Text = "Load";
             this.button3.UseVisualStyleBackColor = true;
@@ -102,17 +108,20 @@
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(914, 600);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.gayBiker);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Made By Auzie#1557";
             ((System.ComponentModel.ISupportInitialize)(this.gayBiker)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
